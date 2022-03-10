c971bc2860433f04f17ca960d0059e0c2a6b2acd
TriLinder <82846064+TriLinder@users.noreply.github.com>
2022-03-10 12:09:22 +0100

    First test of pages

--- controller.py
+++ controller.py
@@ -1,20 +1,23 @@
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
 from PIL import Image, ImageDraw, ImageFont
+import json
 import math
 import time
+import os
 
 class button :
     def __init__(self, keyIndex, controller) :
+        self.controller = controller
         self.keyIndex = keyIndex
 
         #Get the coords of the button. (Top left button is 0x0.)
         self.x = (keyIndex % controller.width)
-        self.y = math.ceil((keyIndex+1) / controller.width)
+        self.y = math.ceil((keyIndex+1) / controller.width)-1
 
         self.coords = f"{self.x}x{self.y}"
 
-        self.caption = f"{self.keyIndex} - {self.coords}"
+        self.caption = ""
         self.fontSize = 14
         self.fontColor = "white"
         self.font = "C:\\Windows\\Fonts\\Arial.ttf"
@@ -34,19 +37,22 @@ class button :
             self.fontColor = color
 
     def sendToDevice(self) :
-        image = PILHelper.create_scaled_image(deck, self.background, margins=[0, 0, 0, 0])
+        image = PILHelper.create_scaled_image(self.controller.deck, self.background, margins=[0, 0, 0, 0])
 
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype(self.font, self.fontSize)
         draw.text((image.width / 2, image.height / 2), text=self.caption, font=font, anchor="ms", fill=self.fontColor)
 
-        image = PILHelper.to_native_format(deck, image)
+        image = PILHelper.to_native_format(self.controller.deck, image)
 
-        with deck :
-            deck.set_key_image(self.keyIndex, image)
+        with self.controller.deck :
+            self.controller.deck.set_key_image(self.keyIndex, image)
     
     def loadImage(self, path) :
         self.background = Image.open(path)
+    
+    def coordsCaption(self) :
+        self.caption = f"{self.keyIndex} - {self.coords}"
 
 class controller :
     def __init__(self, deck) :
@@ -75,8 +81,55 @@ class controller :
     def sendScreenToDevice(self) :
         for key in self.screen :
             self.screen[key].sendToDevice()
+    
+    def setKeyCallback(self, func) :
+        self.deck.set_key_callback(func)
+    
+    def coordsCaptions(self) :
+        for key in self.screen :
+            self.screen[key].coordsCaption()
 
-if __name__ == "__main__" :
+class pages :
+    def __init__(self, controller) :
+        self.pages = {}
+        self.images = {}
+
+        self.controller = controller
+
+        for page in os.listdir("pages") : #Load all the .json files to memory
+            if page.endswith(".json") :
+                with open(os.path.join("pages", page), "r") as f :
+                    self.pages[page] = json.loads(f.read())
+        
+        for page in self.pages : #Load all the used images to memory
+            for image in self.pages[page]["images"] :
+                if not image in self.images :
+                    path = os.path.join("pages", "imgs", image)
+
+                    if os.path.isfile(path) :
+                        self.images[image] = Image.open(path)
+                    else :
+                        print(f"{image} not found!")
+                        self.images[image] = Image.new("RGB", (self.controller.buttonRes, self.controller.buttonRes))
+    
+    def switchToPage(self, page) :
+        j = self.pages[page]
+        buttons = j["buttons"]
+
+        self.controller.coordsCaptions()
+
+        for button in buttons :
+            buttonJ = buttons[button]
+
+            key = self.controller.screen[button]
+            key.setCaption(buttonJ["caption"])
+            key.background = self.images[buttonJ["background"]]
+
+        self.controller.sendScreenToDevice()
+
+
+# ------------------------ #
+def helloWorldTest() :
     streamdecks = DeviceManager().enumerate()
     for index, deck in enumerate(streamdecks) :
 
@@ -84,10 +137,21 @@ if __name__ == "__main__" :
 
         middleKey = c.screen["2x2"]
         middleKey.loadImage("test.png")
-        middleKey.setCaption("Hello, world!")
-        middleKey.setFont("C:\\Windows\\Fonts\\Arial.ttf", size=12, color="red")
+        middleKey.setCaption("Hello,\nworld!")
+        middleKey.setFont("C:\\Windows\\Fonts\\Arial.ttf", size=15, color="red")
 
         c.sendScreenToDevice()
 
         while True :
             time.sleep(10)
+
+# ------------------------ #
+           
+if __name__ == "__main__" :
+    streamdecks = DeviceManager().enumerate()
+
+    for index, deck in enumerate(streamdecks) :
+        c = controller(deck)
+
+        p = pages(c)
+        p.switchToPage("page1.json")
